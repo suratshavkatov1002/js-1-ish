418ed14cfaad3f649e2287b8707fcca1cce0fadd
suratshavkatov1002 <shavkatovsurat321@gmail.com>
2022-07-02 15:23:02 +0500

    vazifa-4

---
+++ main.js
@@ -0,0 +1,30 @@
+var firstName = prompt("Ismingizni kiriting:");
+var lastName = prompt("Familyangizni kiriting:");
+var fullName = prompt("Ism va familyangiz:");
+var yourProfession = prompt("Sizning kasbingiz:");
+var yourAge = prompt("Sizning yoshingiz:");
+var yourYear = prompt ("Tug'ilgan yilingiz:");
+var tellNumber = prompt("RAqamingiz:");
+var passportNumber = prompt("Passport raqamingiz:");
+
+alert
+(`Sizning ismingiz:${firstName}
+Sizning familyangiz: ${lastName}
+Ism familyangiz: ${fullName}
+Kasbingiz: ${yourProfession}
+Yoshingiz: ${yourAge}
+Yilingiz: ${yourYear}
+RAqamingiz:${tellNumber}
+Passport raqamingiz:${passportNumber}
+`)
+
+console.log
+(`Sizning ismingiz:${firstName}
+Sizning familyangiz: ${lastName}
+Ism familyangiz: ${fullName}
+Kasbingiz: ${yourProfession}
+Yoshingiz: ${yourAge}
+Yilingiz: ${yourYear}
+RAqamingiz:${tellNumber}
+Passport raqamingiz:${passportNumber}
+`)
